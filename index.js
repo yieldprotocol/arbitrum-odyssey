bfbdb514ad2405ffd3c4b40e9117df45c8586ff8
marcomariscal <marco.a.mariscal@gmail.com>
2022-08-09 12:47:04 -0700

    feat: get all strategy token holders for arbi dai/usdc june/dec strats

--- index.js
+++ index.js
@@ -1,90 +1,41 @@
 const {
-  AMOUNT_THRESHOLD,
   ZERO_ADDRESS,
-  LADLE,
   START_TIMESTAMP,
   END_TIMESTAMP,
   ONE_DAY,
+  RPC_ENDPOINT,
+  DAI_STRAT,
+  USDC_STRAT,
+  AFFECTED_BLOCK,
 } = require("./constants");
 const { getRes } = require("./helpers");
+const { Contract, ethers } = require("ethers");
+const abi = require("./contracts/abis/Strategy.json");
+const { formatUnits } = require("ethers/lib/utils");
+const { JsonRpcProvider } = require("@ethersproject/providers");
 
 // csv writing data
-const CSV_PATH = "data.csv";
-const CSV_HEADER = [{ id: "account", title: "Account" }];
+const DAI_STRAT_CSV_PATH = "ARBITRUM_YSDAI6MJD.csv";
+const USDC_STRAT_CSV_PATH = "ARBITRUM_YSUSDC6MJD.csv";
+const CSV_HEADER = [
+  { id: "account", title: "Account" },
+  { id: "balance", title: "Balance" },
+];
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
-const csvWriter = createCsvWriter({
-  path: CSV_PATH,
+
+// YSDAI6MJD
+const daiStratWriter = createCsvWriter({
+  path: DAI_STRAT_CSV_PATH,
+  header: CSV_HEADER,
+});
+
+// YSUSDC6MJD
+const usdcStratWriter = createCsvWriter({
+  path: USDC_STRAT_CSV_PATH,
   header: CSV_HEADER,
 });
 
 async function getEligibleUsers() {
-  // proxy for borrowing: when the pool trades from fyToken to base (above threshold) and then base is transferred from ladle to user
-  const _getBorrowers = async () => {
-    let timestamp = START_TIMESTAMP;
-
-    const borrowers = new Set();
-
-    do {
-      const query = `
-    {
-      trades(first:1000, where:{amountFYToken_lte:"-${AMOUNT_THRESHOLD}", from:"${LADLE}", timestamp_gte:${timestamp}, timestamp_lt: ${
-        timestamp + ONE_DAY
-      }}, orderDirection:asc, orderBy:timestamp) {
-        to
-        timestamp
-      }
-     }
-    `;
-
-      const {
-        data: { trades },
-      } = await getRes(query);
-
-      trades.forEach(
-        (trade) => trade.timestamp <= END_TIMESTAMP && borrowers.add(trade.to)
-      );
-
-      // use the latest timestamp as the new timestamp to query
-      timestamp = trades[trades.length - 1].timestamp;
-    } while (timestamp <= END_TIMESTAMP);
-
-    return borrowers;
-  };
-
-  // proxy for lending: some amount of base (above threshold) was traded for fyToken then transferred to user
-  const _getLenders = async () => {
-    let timestamp = START_TIMESTAMP;
-
-    const lenders = new Set();
-
-    do {
-      const query = `
-    {
-      trades(first:1000, where:{amountBaseToken_lte:"-${AMOUNT_THRESHOLD}", timestamp_gte:${timestamp}, timestamp_lt: ${
-        timestamp + ONE_DAY
-      }}, orderDirection:asc, orderBy:timestamp) {
-        to
-        timestamp
-      }
-     }
-    `;
-
-      const {
-        data: { trades },
-      } = await getRes(query);
-
-      trades.forEach(
-        (trade) => trade.timestamp <= END_TIMESTAMP && lenders.add(trade.to)
-      );
-
-      // use the latest timestamp as the new timestamp to query
-      timestamp = trades[trades.length - 1].timestamp;
-    } while (timestamp <= END_TIMESTAMP);
-
-    return lenders;
-  };
-
-  // get instances where strategy tokens were transferred to user
   const _getLiqProviders = async () => {
     let timestamp = START_TIMESTAMP;
 
@@ -92,13 +43,16 @@ async function getEligibleUsers() {
 
     do {
       const query = `
-    {
-      liquidities(first:1000, where:{amountStrategyTokens_gte:"${AMOUNT_THRESHOLD}", from: "${ZERO_ADDRESS}", timestamp_gte:${timestamp}, timestamp_lt: ${
+      {
+        liquidities(first:1000, where:{strategy_in:
+          ["0xe7214af14bd70f6aac9c16b0c1ec9ee1ccc7efda", 
+          "0xdc705fb403dbb93da1d28388bc1dc84274593c11"], 
+           timestamp_gte:${timestamp}, from:"${ZERO_ADDRESS}", timestamp_lt: ${
         timestamp + ONE_DAY
       }}, orderDirection:asc, orderBy:timestamp) {
-        to
-        timestamp
-      }
+          to
+          timestamp
+        }
     }
     `;
 
@@ -112,23 +66,73 @@ async function getEligibleUsers() {
 
       // use the latest timestamp as the new timestamp to query
       timestamp = liquidities[liquidities.length - 1].timestamp;
-    } while (timestamp <= END_TIMESTAMP);
+      console.log("size", liqProviders.size);
+    } while (timestamp < END_TIMESTAMP);
 
-    return liqProviders;
+    const provider = new JsonRpcProvider(RPC_ENDPOINT);
+    const daiStrategy = new Contract(DAI_STRAT, abi, provider);
+    const usdcStrategy = new Contract(USDC_STRAT, abi, provider);
+    const daiDec = await daiStrategy.decimals();
+    const usdcDec = await usdcStrategy.decimals();
+
+    const balances = await Promise.all(
+      [...liqProviders.values()].map(async (lp) => {
+        // YSDAI6MJD
+        let daiStratBal;
+        try {
+          daiStratBal = await daiStrategy.balanceOf(lp, {
+            blockTag: AFFECTED_BLOCK - 1,
+          });
+        } catch (e) {
+          daiStratBal = ethers.constants.Zero;
+        }
+
+        // YSUSDC6MJD
+        let usdcStratBal;
+        try {
+          usdcStratBal = await usdcStrategy.balanceOf(lp, {
+            blockTag: AFFECTED_BLOCK - 1,
+          });
+        } catch (e) {
+          usdcStratBal = ethers.constants.Zero;
+        }
+
+        return {
+          account: lp,
+          daiStratBal: formatUnits(daiStratBal, daiDec),
+          usdcStratBal: formatUnits(usdcStratBal, usdcDec),
+        };
+      })
+    );
+
+    return balances;
   };
 
-  const borrowers = await _getBorrowers();
-  const lenders = await _getLenders();
   const liqProviders = await _getLiqProviders();
 
-  const allUsers = [...borrowers, ...lenders, ...liqProviders].map(
-    (account) => ({ account })
-  );
+  // write to dai strat csv
+  daiStratWriter
+    .writeRecords(
+      liqProviders
+        .filter((lp) => Number(lp.daiStratBal) !== 0)
+        .map((lp) => ({
+          account: lp.account,
+          balance: lp.daiStratBal,
+        }))
+    )
+    .then(() => console.log("The DAI CSV file was written successfully"));
 
-  // write to csv
-  csvWriter
-    .writeRecords(allUsers)
-    .then(() => console.log("The CSV file was written successfully"));
+  // write to usdc strat csv
+  usdcStratWriter
+    .writeRecords(
+      liqProviders
+        .filter((lp) => Number(lp.usdcStratBal) !== 0)
+        .map((lp) => ({
+          account: lp.account,
+          balance: lp.usdcStratBal,
+        }))
+    )
+    .then(() => console.log("The USDC CSV file was written successfully"));
 }
 
 getEligibleUsers();
