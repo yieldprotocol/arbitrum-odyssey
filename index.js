b58bd6206f4eb7770e8545e6d1e691c97c1fc801
marcomariscal <marco.a.mariscal@gmail.com>
2023-03-15 12:17:16 -0700

    feat: get all mainnet borrowers

--- index.js
+++ index.js
@@ -1,134 +1,130 @@
-const {
-  AMOUNT_THRESHOLD,
-  ZERO_ADDRESS,
-  LADLE,
-  START_TIMESTAMP,
-  END_TIMESTAMP,
-  ONE_DAY,
-} = require("./constants");
+const { ZERO_ADDRESS, LADLE, ONE_DAY } = require("./constants");
 const { getRes } = require("./helpers");
 
 // csv writing data
 const CSV_PATH = "data.csv";
-const CSV_HEADER = [{ id: "account", title: "Account" }];
+const CSV_HEADER = [
+  { id: "account", title: "Account" },
+  { id: "seriesId", title: "SeriesId" },
+  { id: "seriesName", title: "SeriesName" },
+  { id: "vaultId", title: "vaultId" },
+  { id: "debtAmountFmt", title: "debtAmountFmt" },
+  { id: "debtAmount", title: "debtAmount" },
+  { id: "collateralAmountFmt", title: "collateralAmountFmt" },
+  { id: "collateralAmount", title: "collateralAmount" },
+  { id: "collateralSymbol", title: "collateralSymbol" },
+];
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const csvWriter = createCsvWriter({
   path: CSV_PATH,
   header: CSV_HEADER,
 });
 
-async function getEligibleUsers() {
-  // proxy for borrowing: when the pool trades from fyToken to base (above threshold) and then base is transferred from ladle to user
-  const _getBorrowers = async () => {
-    let timestamp = START_TIMESTAMP;
+// get all current debt holders by getting vaults
+const getVaults = async () => {
+  let numAccounts = 0;
+  const vaultData = new Set();
+  let skip = 0;
 
-    const borrowers = new Set();
-
-    do {
-      const query = `
-    {
-      trades(first:1000, where:{amountFYToken_lte:"-${AMOUNT_THRESHOLD}", from:"${LADLE}", timestamp_gte:${timestamp}, timestamp_lt: ${
-        timestamp + ONE_DAY
-      }}, orderDirection:asc, orderBy:timestamp) {
-        to
-        timestamp
+  do {
+    const query = `
+      {
+        accounts(where: {vaults_: {debtAmount_gt: "0", series_in: [
+          "0x0030ff00028b", 
+          "0x0031ff00028b", 
+          "0x0032ff00028b", 
+          "0x00a0ff00028b", 
+        ]}}, skip: ${skip}, first: 1000) {
+          vaults {
+            id
+            debtAmount
+            series {
+              fyToken {
+                symbol
+                name
+                decimals
+              }
+              id
+              baseAsset {
+                assetId
+                name
+                symbol
+              }
+            }
+            collateralAmount
+            collateral {
+              asset {
+                symbol
+                name
+                assetId
+                decimals
+              }
+            }
+          }
+          id
+        }
       }
-     }
     `;
 
-      const {
-        data: { trades },
-      } = await getRes(query);
+    const { data } = await getRes(query);
+    const accounts = data ? data.accounts : null;
 
-      trades.forEach(
-        (trade) => trade.timestamp <= END_TIMESTAMP && borrowers.add(trade.to)
-      );
+    if (accounts) {
+      accounts.forEach((a) => {
+        const { id: account, vaults } = a;
 
-      // use the latest timestamp as the new timestamp to query
-      timestamp = trades[trades.length - 1].timestamp;
-    } while (timestamp <= END_TIMESTAMP);
+        vaults.forEach((v) => {
+          const {
+            id: vaultId,
+            debtAmount,
+            collateralAmount,
+            series,
+            collateral,
+          } = v;
+          const {
+            fyToken: { name: seriesName, decimals: fyTokenDecimals },
+            id: seriesId,
+          } = series;
+          const {
+            asset: { decimals: collateralDecimals, symbol: collateralSymbol },
+          } = collateral;
 
-    return borrowers;
-  };
-
-  // proxy for lending: some amount of base (above threshold) was traded for fyToken then transferred to user
-  const _getLenders = async () => {
-    let timestamp = START_TIMESTAMP;
-
-    const lenders = new Set();
-
-    do {
-      const query = `
-    {
-      trades(first:1000, where:{amountBaseToken_lte:"-${AMOUNT_THRESHOLD}", timestamp_gte:${timestamp}, timestamp_lt: ${
-        timestamp + ONE_DAY
-      }}, orderDirection:asc, orderBy:timestamp) {
-        to
-        timestamp
-      }
-     }
-    `;
-
-      const {
-        data: { trades },
-      } = await getRes(query);
-
-      trades.forEach(
-        (trade) => trade.timestamp <= END_TIMESTAMP && lenders.add(trade.to)
-      );
-
-      // use the latest timestamp as the new timestamp to query
-      timestamp = trades[trades.length - 1].timestamp;
-    } while (timestamp <= END_TIMESTAMP);
-
-    return lenders;
-  };
-
-  // get instances where strategy tokens were transferred to user
-  const _getLiqProviders = async () => {
-    let timestamp = START_TIMESTAMP;
-
-    const liqProviders = new Set();
-
-    do {
-      const query = `
-    {
-      liquidities(first:1000, where:{amountStrategyTokens_gte:"${AMOUNT_THRESHOLD}", from: "${ZERO_ADDRESS}", timestamp_gte:${timestamp}, timestamp_lt: ${
-        timestamp + ONE_DAY
-      }}, orderDirection:asc, orderBy:timestamp) {
-        to
-        timestamp
-      }
+          if (
+            seriesName.includes("2306") &&
+            +debtAmount > 0 &&
+            +collateralAmount !== +debtAmount // filter out vaults associated with adding liquidity
+          ) {
+            vaultData.add({
+              account,
+              seriesId,
+              seriesName,
+              vaultId,
+              debtAmountFmt: debtAmount,
+              debtAmount: (+debtAmount * 10 ** fyTokenDecimals).toString(),
+              collateralAmountFmt: collateralAmount,
+              collateralAmount: (
+                +collateralAmount *
+                10 ** collateralDecimals
+              ).toString(),
+              collateralSymbol,
+            });
+          }
+        });
+      });
     }
-    `;
 
-      const {
-        data: { liquidities },
-      } = await getRes(query);
-
-      liquidities.forEach(
-        (liq) => liq.timestamp <= END_TIMESTAMP && liqProviders.add(liq.to)
-      );
-
-      // use the latest timestamp as the new timestamp to query
-      timestamp = liquidities[liquidities.length - 1].timestamp;
-    } while (timestamp <= END_TIMESTAMP);
-
-    return liqProviders;
-  };
-
-  const borrowers = await _getBorrowers();
-  const lenders = await _getLenders();
-  const liqProviders = await _getLiqProviders();
-
-  const allUsers = [...borrowers, ...lenders, ...liqProviders].map(
-    (account) => ({ account })
-  );
+    skip += 1000;
+    numAccounts += accounts ? accounts.length : 0;
+    console.log(
+      "🦄 ~ file: index.js:88 ~ getVaults ~ numAccounts:",
+      numAccounts
+    );
+  } while (skip <= 10000); // tries to get first 10000ish accounts
 
   // write to csv
   csvWriter
-    .writeRecords(allUsers)
+    .writeRecords(vaultData)
     .then(() => console.log("The CSV file was written successfully"));
-}
+};
 
-getEligibleUsers();
+getVaults();
